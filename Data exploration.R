6e1540399b016d796a02346a7fc5a2e14bc64ef2
Arnold-Kakas <kakasarnold@gmail.com>
2023-09-22 09:28:15 +0200

    xgb

--- Data exploration.R
+++ Data exploration.R
@@ -10,8 +10,8 @@ test <- read.csv("Data/test.csv")
 submission_id <- read.csv("Data/test.csv") %>% select(id)
 
 # Train data wrangling
-train <- train %>% mutate_if(is.character, ~na_if(., ""))
-train <- train %>% mutate_if(is.numeric, ~na_if(., 0))
+
+train[is.na(train)] <- 0
 
 train <- train %>% 
   select(-c(
@@ -25,34 +25,53 @@ train <- train %>%
     std_ic_t2o_mou_6, # all blank or 0
     std_ic_t2o_mou_7, # all blank or 0
     std_ic_t2o_mou_8, # all blank or 0
-    date_of_last_rech_6, # nothing usefull
-    date_of_last_rech_7, # nothing usefull
-    date_of_last_rech_8, # nothing usefull
-    last_day_rch_amt_6, # nothing usefull
-    last_day_rch_amt_7, # nothing usefull
-    last_day_rch_amt_8, # nothing usefull
-    date_of_last_rech_data_6, # nothing usefull
-    date_of_last_rech_data_7, # nothing usefull
-    date_of_last_rech_data_8, # nothing usefull
-    #last_date_of_month_6, # nothing usefull
-    #last_date_of_month_7, # nothing usefull
-    #last_date_of_month_8, # nothing usefull
-    total_og_mou_6, # totals will be calculated later on
-    total_og_mou_7, # totals will be calculated later on
-    total_og_mou_8, # totals will be calculated later on
-    total_ic_mou_6, # totals will be calculated later on
-    total_ic_mou_7, # totals will be calculated later on
-    total_ic_mou_8, # totals will be calculated later on
-    total_rech_num_6, # totals will be calculated later on
-    total_rech_num_7, # totals will be calculated later on
-    total_rech_num_8, # totals will be calculated later on
-    total_rech_amt_6, # totals will be calculated later on
-    total_rech_amt_7, # totals will be calculated later on
-    total_rech_amt_8, # totals will be calculated later on
-    total_rech_data_6, # totals will be calculated later on
-    total_rech_data_7, # totals will be calculated later on
-    total_rech_data_8 # totals will be calculated later on
+    #date_of_last_rech_6, # nothing usefull
+    #date_of_last_rech_7, # nothing usefull
+    #date_of_last_rech_8, # nothing usefull
+    #last_day_rch_amt_6, # nothing usefull
+    #last_day_rch_amt_7, # nothing usefull
+    #last_day_rch_amt_8, # nothing usefull
+    #date_of_last_rech_data_6, # nothing usefull
+    #date_of_last_rech_data_7, # nothing usefull
+    #date_of_last_rech_data_8, # nothing usefull
+    last_date_of_month_6, # nothing usefull
+    last_date_of_month_7, # nothing usefull
+    last_date_of_month_8, # nothing usefull
+    #total_og_mou_6, # totals will be calculated later on
+    #total_og_mou_7, # totals will be calculated later on
+    #total_og_mou_8, # totals will be calculated later on
+    #total_ic_mou_6, # totals will be calculated later on
+    #total_ic_mou_7, # totals will be calculated later on
+    #total_ic_mou_8, # totals will be calculated later on
+    #total_rech_num_6, # totals will be calculated later on
+    #total_rech_num_7, # totals will be calculated later on
+    #total_rech_num_8, # totals will be calculated later on
+    #total_rech_amt_6, # totals will be calculated later on
+    #total_rech_amt_7, # totals will be calculated later on
+    #total_rech_amt_8, # totals will be calculated later on
+    #total_rech_data_6, # totals will be calculated later on
+    #total_rech_data_7, # totals will be calculated later on
+    #total_rech_data_8 # totals will be calculated later on
   ))
+
+# train <- train %>% 
+#   mutate(date_of_last_rech_6 = as.POSIXct(mdy(date_of_last_rech_6)),
+#          date_of_last_rech_7 = as.POSIXct(mdy(date_of_last_rech_7)),
+#          date_of_last_rech_8 = as.POSIXct(mdy(date_of_last_rech_8)),
+#          date_of_last_rech_data_6 = as.POSIXct(mdy(date_of_last_rech_data_6)),
+#          date_of_last_rech_data_7 = as.POSIXct(mdy(date_of_last_rech_data_7)),
+#          date_of_last_rech_data_8 = as.POSIXct(mdy(date_of_last_rech_data_8)),
+#          last_day_rch_amt_6 = as.POSIXct(mdy(last_day_rch_amt_6)),
+#          last_day_rch_amt_7 = as.POSIXct(mdy(last_day_rch_amt_7)),
+#          last_day_rch_amt_8 = as.POSIXct(mdy(last_day_rch_amt_8)))
+
+train <- train %>% 
+  mutate(date_of_last_rech_6 = if_else(date_of_last_rech_6 == "", 0, 1),
+         date_of_last_rech_7 = if_else(date_of_last_rech_7 == "", 0, 1),
+         date_of_last_rech_8 = if_else(date_of_last_rech_8 == "", 0, 1),
+         date_of_last_rech_data_6 = if_else(date_of_last_rech_data_6 == "", 0, 1),
+         date_of_last_rech_data_7 =if_else(date_of_last_rech_data_7 == "", 0, 1),
+         date_of_last_rech_data_8 = if_else(date_of_last_rech_data_8 == "", 0, 1))
 
 train <- train %>% 
   rename(
@@ -65,7 +84,13 @@ train <- train %>%
   pivot_longer(
     cols = -c(id,
               churn,
-              aon)) %>% 
+              aon,
+              date_of_last_rech_6,
+              date_of_last_rech_7,
+              date_of_last_rech_8,
+              date_of_last_rech_data_6,
+              date_of_last_rech_data_7,
+              date_of_last_rech_data_8)) %>% 
   mutate(month_id = str_extract(name, ".$"),
          month_id = case_when(
            month_id == "6" ~ "Jun",
@@ -99,25 +124,22 @@ train[is.na(train)] <- 0
 
 # Final dataset
 train_cleaned <- train %>% 
-  mutate(
-    #circle = as.factor(circle),
-    churn = as.factor(churn),
-    fb_user = as.factor(fb_user)
-    ) %>% 
+  mutate(churn = as.factor(churn)) %>% 
+  # mutate( #xgboost does not allow factors
+  #   #circle = as.factor(circle),
+  #   fb_user = as.factor(fb_user),
+  #   date_of_last_rech_6 = as.factor(date_of_last_rech_6),
+  #   date_of_last_rech_7 = as.factor(date_of_last_rech_7),
+  #   date_of_last_rech_8 = as.factor(date_of_last_rech_8),
+  #   date_of_last_rech_data_6 = as.factor(date_of_last_rech_data_6),
+  #   date_of_last_rech_data_7 = as.factor(date_of_last_rech_data_7),
+  #   date_of_last_rech_data_8 = as.factor(date_of_last_rech_data_8)
+  #   ) %>% 
   select(-id)
 
 
 # Create a tidymodels recipe for data preprocessing
 model_recipe <- recipe(churn ~ ., data = train_cleaned) %>%
-  step_date(c(    date_of_last_rech_6, 
-                  date_of_last_rech_7, 
-                  date_of_last_rech_8, 
-                  last_day_rch_amt_6, 
-                  last_day_rch_amt_7, 
-                  last_day_rch_amt_8, 
-                  date_of_last_rech_data_6, 
-                  date_of_last_rech_data_7, 
-                  date_of_last_rech_data_8 ), features = c("month")) %>% 
   step_scale(all_numeric_predictors()) %>%
   step_center(all_numeric_predictors())# %>% 
   #step_pca(all_numeric_predictors())
@@ -125,84 +147,139 @@ model_recipe <- recipe(churn ~ ., data = train_cleaned) %>%
 
 # Split the data into training and testing sets
 set.seed(123)  # For reproducibility
-data_split <- initial_split(train_cleaned, prop = 0.7)
+data_split <- initial_split(train_cleaned, prop = 0.7, strata = churn)
 train_data <- training(data_split)
 test_data <- testing(data_split)
 
 
 # Lets see how do PCAs look like, for reference check https://juliasilge.com/blog/cocktail-recipes-umap/
 
-pca_prep <- prep(model_recipe)
+# pca_prep <- prep(model_recipe)
+# 
+# tidied_pca <- tidy(pca_prep, 2)
+# 
+# tidied_pca %>%
+#   filter(component %in% paste0("PC", 1:5)) %>%
+#   mutate(component = fct_inorder(component)) %>%
+#   ggplot(aes(value, terms, fill = terms)) +
+#   geom_col(show.legend = FALSE) +
+#   facet_wrap(~component, nrow = 1) +
+#   labs(y = NULL)
 
-tidied_pca <- tidy(pca_prep, 2)
-
-tidied_pca %>%
-  filter(component %in% paste0("PC", 1:5)) %>%
-  mutate(component = fct_inorder(component)) %>%
-  ggplot(aes(value, terms, fill = terms)) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~component, nrow = 1) +
-  labs(y = NULL)
-
-# Fit the recipe to the training data
-data_preprocessor <- prep(model_recipe, training = train_data)
+ # Fit the recipe to the training data
+# data_preprocessor <- prep(model_recipe, training = train_data)
 
 # Transform both training and testing data
-train_data_preprocessed <- bake(data_preprocessor, new_data = train_data)
-test_data_preprocessed <- bake(data_preprocessor, new_data = test_data)
+# train_data_preprocessed <- bake(data_preprocessor, new_data = train_data)
+# test_data_preprocessed <- bake(data_preprocessor, new_data = test_data)
 
-# Build your Random Forest model using tidymodels
-model_spec <- rand_forest() %>%
-  set_engine("randomForest") %>%
+# XG boost model
+
+xgb_spec <- boost_tree(
+  trees = 1000,
+  tree_depth = tune(), min_n = tune(),
+  loss_reduction = tune(),                     ## first three: model complexity
+  sample_size = tune(), mtry = tune(),         ## randomness
+  learn_rate = tune()                          ## step size
+) %>%
+  set_engine("xgboost") %>%
   set_mode("classification")
 
-# Fit the model to the data
-final_model <- model_spec %>%
-  fit(churn ~ ., data = train_data_preprocessed)
-
-tune_spec <- rand_forest(
-  mtry = tune(),
-  trees = 1000,
-  min_n = tune()
-) %>%
-  set_mode("classification") %>%
-  set_engine("ranger")
-
-tune_wf <- workflow() %>%
-  add_recipe(model_recipe) %>%
-  add_model(tune_spec)
-
-trees_folds <- vfold_cv(train_data)
-
-tune_res <- tune_grid(
-  tune_wf,
-  resamples = trees_folds,
-  grid = 20
+xgb_grid <- grid_latin_hypercube(
+  tree_depth(),
+  min_n(),
+  loss_reduction(),
+  sample_size = sample_prop(),
+  finalize(mtry(), train_data),
+  learn_rate(),
+  size = 30
 )
 
-tune_res %>%
-  collect_metrics() %>%
-  filter(.metric == "roc_auc") %>%
-  select(mean, min_n, mtry) %>%
-  pivot_longer(min_n:mtry,
-               values_to = "value",
-               names_to = "parameter"
-  ) %>%
-  ggplot(aes(value, mean, color = parameter)) +
-  geom_point(show.legend = FALSE) +
-  facet_wrap(~parameter, scales = "free_x") +
-  labs(x = NULL, y = "AUC")
-
-# Make predictions on the test set
-predictions <- final_model %>%
-  predict(new_data = test_data_preprocessed) %>%
-  pull(.pred_class)
-
-# Evaluate your model's performance (e.g., accuracy)
-accuracy <- mean(predictions == test_data_preprocessed$churn)
-print(paste("Accuracy:", accuracy))
+xgb_wf <- workflow() %>%
+  add_recipe(model_recipe) %>% 
+  add_model(xgb_spec)
 
 
+set.seed(123)
+vb_folds <- vfold_cv(train_data, strata = churn)
+
+doParallel::registerDoParallel()
+
+set.seed(234)
+xgb_res <- tune_grid(
+  xgb_wf,
+  resamples = vb_folds,
+  grid = xgb_grid,
+  control = control_grid(save_pred = TRUE)
+)
+
+
+
+show_best(xgb_res, "accuracy")
+
+
+best_accuracy <- select_best(xgb_res, "accuracy")
+
+final_xgb <- finalize_workflow(
+  xgb_wf,
+  best_auc
+)
+
+# # # Build your Random Forest model using tidymodels
+# # model_spec <- rand_forest() %>%
+# #   set_engine("randomForest") %>%
+# #   set_mode("classification")
+# # 
+# # # Fit the model to the data
+# # final_model <- model_spec %>%
+# #   fit(churn ~ ., data = train_data_preprocessed)
+# # 
+# # tune_spec <- rand_forest(
+# #   mtry = tune(),
+# #   trees = 1000,
+# #   min_n = tune()
+# # ) %>%
+# #   set_mode("classification") %>%
+# #   set_engine("ranger")
+# # 
+# # tune_wf <- workflow() %>%
+# #   add_recipe(model_recipe) %>%
+# #   add_model(tune_spec)
+# # 
+# # trees_folds <- vfold_cv(train_data)
+# # 
+# # tune_res <- tune_grid(
+# #   tune_wf,
+# #   resamples = trees_folds,
+# #   grid = 20
+# # )
+# # 
+# # tune_res %>%
+# #   collect_metrics() %>%
+# #   filter(.metric == "roc_auc") %>%
+# #   select(mean, min_n, mtry) %>%
+# #   pivot_longer(min_n:mtry,
+# #                values_to = "value",
+# #                names_to = "parameter"
+# #   ) %>%
+# #   ggplot(aes(value, mean, color = parameter)) +
+# #   geom_point(show.legend = FALSE) +
+# #   facet_wrap(~parameter, scales = "free_x") +
+# #   labs(x = NULL, y = "AUC")
+# 
+# # Make predictions on the test set
+# predictions <- final_model %>%
+#   predict(new_data = test_data_preprocessed) %>%
+#   pull(.pred_class)
+# 
+# # Evaluate your model's performance (e.g., accuracy)
+# accuracy <- mean(predictions == test_data_preprocessed$churn)
+# print(paste("Accuracy:", accuracy))
+
+
+final_res <- last_fit(final_xgb, data_split)
+
+final_res
 #####################################################################
 #############     TEST DATASET
 #####################################################################
